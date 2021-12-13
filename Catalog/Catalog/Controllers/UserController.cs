2f8d2f3ae810305ae59cf2a2d8646354bac8aa47
nepodeveloper <neponed@gmail.com>
2021-12-13 16:39:20 +0200

    Jwt Token update, method validation with token and token generation.

--- Catalog/Catalog/Controllers/UserController.cs
+++ Catalog/Catalog/Controllers/UserController.cs
@@ -1,12 +1,19 @@
 ﻿using Catalog.BindingModel;
 using Catalog.Data.Entities;
 using Catalog.DataTransferObject;
+using Catalog.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Catalog.Controllers
@@ -18,13 +25,16 @@ namespace Catalog.Controllers
         private readonly ILogger<UserController> _logger;
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly JWTConfig jWTConfig;
 
-        public UserController(ILogger<UserController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public UserController(ILogger<UserController> logger, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IOptions<JWTConfig> options)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            jWTConfig = options.Value;
             _logger = logger;
         }
+        
         [HttpPost("RegisterUser")]
         public async Task<object> RegisterUSer([FromBody] AddUpdateRegisterUserBindingModel model)
         {
@@ -46,6 +56,7 @@ namespace Catalog.Controllers
 
         }
 
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [HttpGet("GetAllUser")]
         public async Task<object> GetAllUser()
         {
@@ -57,6 +68,55 @@ namespace Catalog.Controllers
             {
                 return await Task.FromResult(ex.Message);
             }
+        }
+
+        [HttpPost("login")]
+
+        public async Task<object> Login([FromBody] LoginBindingModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var results = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+                    if (results.Succeeded)
+                    {
+                        var appUser = await _userManager.FindByEmailAsync(model.Email);
+                        var user = new UserDTO(appUser.FullName, appUser.Email, appUser.UserName, appUser.DateCreated);
+                        user.Token = GenerateToken(appUser);
+                        return await Task.FromResult(user);
+                    }
+                }
+               
+                return await Task.FromResult("invalid details");
+               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private string GenerateToken(AppUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(jWTConfig.Key);
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new[]
+                {
+                    new System.Security.Claims.Claim(JwtRegisteredClaimNames.NameId,user.Id),
+                    new System.Security.Claims.Claim(JwtRegisteredClaimNames.Email,user.Email),
+                    new System.Security.Claims.Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                }),
+                Expires=DateTime.UtcNow.AddHours(12),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256),
+                Audience = jWTConfig.Audience,
+                Issuer = jWTConfig.Issuer
+            };
+            var token = tokenHandler.CreateToken(tokenDescription);
+            return tokenHandler.WriteToken(token);
         }
     }
 
